ab7c915011b415c7e44a9f1554e0787e550e3d65
Ricky Park <serenade4yu@gmail.com>
2015-10-04 21:32:27 +0900

    add settings.swift

---
+++ NPETSettingsViewController.swift
@@ -0,0 +1,9 @@
+//
+//  NPETSettingsViewController.swift
+//  NowPlayingEveryTime
+//
+//  Created by 박성민 on 2015. 8. 23..
+//  Copyright (c) 2015년 RickyPark. All rights reserved.
+//
+
+import Foundation
