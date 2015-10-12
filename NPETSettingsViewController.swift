55ea52200159b2382f2d459d07cb1729cec2d7bb
Ricky Park <serenade4yu@gmail.com>
2015-10-12 21:32:29 +0900

    1. add contents file ("SettingsList.plist") 2. display contents data on the table view 3. if select "Select Divider" then let users select one of "_", "/", or "-". Save the divider user selected in the NSUserDefaults. 4. set swipe property as "Left"

--- NPETSettingsViewController.swift
+++ NPETSettingsViewController.swift
@@ -10,7 +10,9 @@ import UIKit
 
 public class NPETSettingsViewController : UITableViewController {
     
-    var settingsData:NSDictionary = NSDictionary()
+    let SettingsCellIdentifier:String = "SettingsCellIdentifier"
+    
+    var settingsDict:NSDictionary = NSDictionary()
     
     
     public override func viewDidLoad() {
@@ -26,6 +28,67 @@ public class NPETSettingsViewController : UITableViewController {
     }
     
     private func getSettingsDataFromPlist() {
-        tableView!.reloadData()
+        
+        if let settingsFilePath:String = NSBundle.mainBundle().pathForResource("SettingsList", ofType: "plist") {
+            settingsDict = NSDictionary(contentsOfFile: settingsFilePath)!
+        }
+        
+        if settingsDict.count > 0 {
+            tableView!.reloadData()
+        }
+    }
+    
+    public override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return settingsDict.count
+    }
+    
+    public override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell:UITableViewCell?
+        cell = (tableView.dequeueReusableCellWithIdentifier(SettingsCellIdentifier) as UITableViewCell?)
+        
+        if cell == nil {
+            cell = UITableViewCell(style: .Subtitle, reuseIdentifier: SettingsCellIdentifier)
+        }
+        
+        for settingsKey in settingsDict.allKeys {
+            let parsedKey = (settingsKey.componentsSeparatedByString(". ") as Array)
+            if Int(parsedKey.first!) == (indexPath.row + 1) {
+                cell!.textLabel?.text = parsedKey.last
+            }
+        }
+        
+        return cell!;
+    }
+    
+    func someHandler(alert: UIAlertAction!) {
+        NSUserDefaults.standardUserDefaults().setObject(alert.title!, forKey: "NPETDivider")
+        
+        let titleString:String = String(format: "Divider is set to %@", arguments: [alert.title!])
+        UIAlertView(title: titleString, message: nil, delegate: nil, cancelButtonTitle: "Okay").show()
+    }
+    
+    public override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        
+        switch indexPath.row {
+        case 0:
+            // show action sheet to select one of "_", "-", or "/". Default value is "_".
+            let selectDividerActionSheet:UIAlertController = UIAlertController(title: "Select one in this list", message: nil, preferredStyle: .ActionSheet)
+            let underscoreSelectedAction:UIAlertAction = UIAlertAction(title: "_", style: UIAlertActionStyle.Default, handler: someHandler)
+            let slashSelectedAction:UIAlertAction = UIAlertAction(title: "/", style: UIAlertActionStyle.Default, handler: someHandler)
+            let hyphenSelectedAction:UIAlertAction = UIAlertAction(title: "-", style: UIAlertActionStyle.Default, handler: someHandler)
+            
+            selectDividerActionSheet.addAction(underscoreSelectedAction)
+            selectDividerActionSheet.addAction(slashSelectedAction)
+            selectDividerActionSheet.addAction(hyphenSelectedAction)
+            
+            self.presentViewController(selectDividerActionSheet, animated: true, completion:nil)
+            
+            break;
+        case 1:
+            break;
+        default:
+            break;
+        }
     }
 }
